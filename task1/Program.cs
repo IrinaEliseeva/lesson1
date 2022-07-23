1f20085e5e3db207dd4926dce24dcdf47620aeec
Irina Eliseeva <madamlindalopez@yandex.ru>
2022-07-23 11:51:31 +0300

    first

---
+++ task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, 22022 World!");
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int a = 5;
+int b = 7;
+Console.WriteLine("введите два числа");
+
+string? sa = Console.ReadLine();
+string? sb = Console.ReadLine();
+
+int.TryParse(
+    sa,
+    System.Globalization.NumberStyles.Integer,
+    null,
+    out a
+);
+
+int.TryParse(
+    sb,
+    System.Globalization.NumberStyles.Integer,
+    null,
+    out b
+);
+
+Console.Write("максимальное число: ");
+
+if (a > b)
+{
+    Console.Write(a);
+}
+else
+{
+    Console.Write(b);
+}
